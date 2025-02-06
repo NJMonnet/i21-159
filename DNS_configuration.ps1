18acab393886acd5e757d6c88d7563e57eb3101e
NJMonnet <github@monnet.swiss>
2025-02-06 08:40:15 +0100

    Update DNS_configuration.ps1

--- DNS_configuration.ps1
+++ DNS_configuration.ps1
@@ -15,6 +15,3 @@ $DnsServer1 = "9.9.9.9"
 $DnsServer2 = "1.1.1.1"
 
 Set-DnsServerForwarder -IPAddress $DnsServer1, $DnsServer2
-
-# Installation du service AD avec niveau fonctionnel 2008
-# https://www.technig.com/install-active-directory-on-windows-server-2022-using-powershell/
