90daac0fc9ac1e2d1121dc3ec3aec921176c3d28
NJMonnet <github@monnet.swiss>
2025-02-06 09:13:40 +0100

    Update AD_DS_configuration.ps1

--- AD_DS_configuration.ps1
+++ AD_DS_configuration.ps1
@@ -1,12 +1,8 @@
 # Installation du service AD DS
+# https://learn.microsoft.com/en-us/windows-server/identity/ad-ds/deploy/install-active-directory-domain-services--level-100-#BKMK_PS
 $NetBIOS = "ICT159"
 $DomainName = "ICT159-private"
 $SafeModePassword = ConvertTo-SecureString -AsPlainText "Welcome2024" -Force
 
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
-Install-ADDSForest -DomainName $DomainName 
-                    -DomainNetbiosName $NetBios 
-                    -ForestMode "Win2008R2" 
-                    -DomainMode "Win2008R2" 
-                    -InstallDNS:$true
-                    -SafeModeAdministratorPassword $SafeModePassword
+Install-ADDSForest -DomainName $DomainName -DomainNetbiosName $NetBios -ForestMode "Win2008R2" -DomainMode "Win2008R2" -InstallDNS:$false -SafeModeAdministratorPassword $SafeModePassword
