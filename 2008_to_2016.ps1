5d0e49c2347045c10d87dc5dac74919c80d92d4f
NJMonnet <github@monnet.swiss>
2025-02-06 10:58:29 +0100

    Update 2008_to_2016.ps1

--- 2008_to_2016.ps1
+++ 2008_to_2016.ps1
@@ -1,4 +1,6 @@
 # Mise à niveau de l'AD vers 2016
+# https://learn.microsoft.com/en-us/powershell/module/activedirectory/set-adforestmode?view=windowsserver2025-ps
+# https://learn.microsoft.com/en-us/powershell/module/activedirectory/set-addomainmode?view=windowsserver2025-ps
 
 Set-ADForestMode -Identity "ICT159.private" -ForestMode Windows2016Forest
 Set-ADDomainMode -Identity "ICT159.private" -DomainMode Windows2016Domain
