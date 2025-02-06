e75bb753a6e05de4815607f9fe1b5d7719c0fd48
NJMonnet <github@monnet.swiss>
2025-02-06 09:19:52 +0100

    Update AD_DS_configuration.ps1

--- AD_DS_configuration.ps1
+++ AD_DS_configuration.ps1
@@ -1,7 +1,7 @@
 # Installation du service AD DS
 # https://learn.microsoft.com/en-us/windows-server/identity/ad-ds/deploy/install-active-directory-domain-services--level-100-#BKMK_PS
 $NetBIOS = "ICT159"
-$DomainName = "ICT159-private"
+$DomainName = "ICT159.private"
 $SafeModePassword = ConvertTo-SecureString -AsPlainText "Welcome2024" -Force
 
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
